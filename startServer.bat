829ca2cbee3c8af0712e3c8cdfd487c8ac85f706
Zhimeng Jin <canglangke001@gmail.com>
2015-09-14 12:15:11 +1000

    Add 10 seconds wait time in start/stop JBoss scripts.

--- startServer.bat
+++ startServer.bat
@@ -1,3 +1,4 @@
 @echo Starting JBoss Server ...
+ping 1.1.1.1 -n 1 -w 10000 > nul
 net start JBossEAP6
 @echo JBoss Server started.
--- stopServer.bat
+++ stopServer.bat
@@ -1,3 +1,4 @@
 @echo Stopping JBoss Server ...
 net stop JBossEAP6
+ping 1.1.1.1 -n 1 -w 10000 > nul
 @echo JBoss Server Stopped.
